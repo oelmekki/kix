adaf7bb784e6594fac9a5b5dd41f61925ca38a60
Olivier El Mekki <oelmekki@gmail.com>
2012-03-11 13:51:22 +0100

    ADD border and canvas context save wrapper

--- client_side/Drawer.js
+++ client_side/Drawer.js
@@ -10,17 +10,35 @@ var Drawer = new Class({
 
   update: function( players ){
     this.ctx.clearRect( 0, 0, configuration.area_width, configuration.area_height );
+    this.drawBorder();
     Object.each( players, function( player ){
-      this.draw( player );
+      this.drawPlayer( player );
     }.bind( this ));
   },
 
 
   // Protected
+  
+
+  draw: function( drawing_function, params ){
+    this.ctx.save();
+    drawing_function.call( this, params );
+    this.ctx.restore();
+  }.protect(),
 
 
-  draw: function( player ){
-    this.ctx.fillStyle = player.color;
-    this.ctx.fillRect( player.x, player.y, player.width, player.height );
+  drawPlayer: function( player ){
+    this.draw( function( player ){
+      this.ctx.fillStyle = player.color;
+      this.ctx.fillRect( player.x, player.y, player.width, player.height );
+    }, player );
+  }.protect(),
+
+
+  drawBorder: function(){
+    this.draw( function(){
+      this.ctx.strokeStyle = '#000000';
+      this.ctx.strokeRect( 0, 0, configuration.area_width, configuration.area_height );
+    });
   }.protect()
 });
